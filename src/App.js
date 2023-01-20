cf11f7bdacd855e484d40029769f8ae7ffa81c76
forNerzul <sbeardman92@gmail.com>
2023-01-19 21:01:59 -0300

    checkpoint commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ColorForm = (props) => {
+    return (
+        <div>
+            <form>
+                <label htmlFor="color">Ingrese un color: </label>
+                <input type="text" name="color" />
+            </form>
+        </div>
+    );
+};
+
+const Blocks = (props) => {
+    const blocks = [];
+    blocks.push(<Block color="red" />);
+};
+
+const Block = (props) => {
+    return (
+        <div
+            style={{
+                backgroundColor: props.color,
+                width: "200px",
+                height: "200px",
+            }}
+        ></div>
+    );
+};
+
+const App = () => {
+    const [colors, setColors] = useState([]);
+
+    const agregarColor = (color) => {
+        setColors([...colors, color]);
+    };
+
+    return <ColorForm agregarColor={agregarColor} />;
+};
 
 export default App;
